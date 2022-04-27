a36dd028c367e961296c1432fc2e98b24e7fe2ca
demon00111 <dap.globaliasoft@gmail.com>
2022-04-27 12:11:48 +0530

    stripe_payment

---
+++ payment/models.py
@@ -0,0 +1,25 @@
+from email.mime import image
+from pickle import TRUE
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100,null=TRUE)
+    currency = models.CharField(max_length=100,null=TRUE)
+    amount = models.CharField(max_length=100,null=TRUE)
+    img = models.ImageField(null = TRUE)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    name = models.CharField(max_length=100,null=TRUE)
+    quantity = models.CharField(max_length=100,null=TRUE)
+    currency = models.CharField(max_length=100,null=TRUE)
+    amount = models.CharField(max_length=100,null=TRUE)
+
+    def __str__(self):
+        return self.name
